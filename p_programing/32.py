a54ccfa6ce92d8e03d8c31ee2ad39ba01bf90538
keyleeu <minhk.lee21@gmail.com>
2023-04-12 23:27:08 +0900

    #4/12 11:27pm

--- p_programing/32.py
+++ p_programing/32.py
@@ -7,13 +7,16 @@ input = sys.stdin.readline
 
 n = int(input())
 n_list = [] * n
-for i in range(n):
+for _ in range(n):
     a = int(input())
     n_list.append(a)
 
+# n_list = list(map(int, input()))
+# n_list = [map(int, input()) for i in range(n)]
 # print(n_list)
 
 n_list.sort()
+# n_list = sorted(n_list,e)
 
 for i in range(n):
     print(n_list[i])
